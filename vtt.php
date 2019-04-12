cbc6de25833eba45bb592d4c923637aade23777c
asusGuy94 <asusGuy94.1@gmail.com>
2019-04-12 02:40:33 +0200

    File now checks if the video exists
    If the video does not exist, the function would give an error

--- vtt.php
+++ vtt.php
@@ -7,7 +7,7 @@ $query = $pdo->prepare("SELECT * FROM videos");
 $query->execute();
 
 foreach ($query->fetchAll() AS $video) {
-	if (!file_exists("vtt/$video[id].vtt")) {
+	if (file_exists("videos/$video[path]") && !file_exists("vtt/$video[id].vtt")) {
 		$vtt->generateVtt($video['path'], $video['id']);
 	}
 }
