486e1b3962c804c193888726ca3a92c989cb9cb6
asusGuy94 <asusGuy94.1@gmail.com>
2020-07-22 13:54:24 +0200

    Added star-form section

--- src/components/pages/star.jsx
+++ src/components/pages/star.jsx
@@ -34,12 +34,113 @@ class StarVideos extends Component {
     }
 }
 
+class StarFormInput extends Component {
+    state = {
+        input: {
+            value: '',
+            id: '',
+        },
+    }
+
+    updateValue(e) {
+        let inputValue = e.target.value
+        let inputID = e.target.id
+
+        this.setState((prevState) => {
+            let input = prevState.input
+            input.id = inputID
+            input.value = inputValue
+
+            return { input }
+        })
+    }
+
+    keyPress(e) {
+        if (e.key === 'Enter') {
+            if (this.state.input.id.length && this.state.input.value.length) {
+                this.props.update(this.state.input.id, this.state.input.value)
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className='input-wrapper'>
+                <label htmlFor={this.props.name.toLowerCase()}>{this.props.name}</label>
+                <input
+                    type={this.props.type}
+                    id={this.props.name.toLowerCase()}
+                    defaultValue={this.props.value}
+                    onChange={(e) => this.updateValue(e)}
+                    onKeyDown={(e) => this.keyPress(e)}
+                />
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+class StarForm extends Component {
+    constructor(props) {
+        super(props)
+        this.update = (label, value) => props.update(label, value)
+    }
+
+    render() {
+        const { data } = this.props
+
+        return (
+            <React.Fragment>
+                <div className='action'>
+                    <div className='btn btn-primary' id='freeones'>
+                        Get Data
+                    </div>
+
+                    <div className='btn btn-outline-secondary' id='freeones_rs'>
+                        Reset Data
+                    </div>
+                </div>
+
+                <StarFormInput update={this.update} name='Breast' value={data.breast} />
+                <StarFormInput update={this.update} name='EyeColor' value={data.eyecolor} />
+                <StarFormInput update={this.update} name='HairColor' value={data.haircolor} />
+                <StarFormInput update={this.update} name='HairColor' value={data.ethnicity} />
+                <StarFormInput update={this.update} name='Country' value={data.country.name}>
+                    <i className={`flag flag-${data.country.code}`} />
+                </StarFormInput>
+                <StarFormInput update={this.update} name='Birthdate' value={data.birthdate} />
+                <StarFormInput update={this.update} name='Height' value={data.height} />
+                <StarFormInput update={this.update} name='Weight' value={data.weight} />
+                <StarFormInput update={this.update} name='Start' value={data.year.start} />
+                <StarFormInput update={this.update} name='End' value={data.year.end} />
+            </React.Fragment>
+        )
+    }
+}
+
 class Star extends Component {
     state = {
         star: {
             id: 0,
             name: '',
             image: '',
+            info: {
+                breast: '',
+                eyecolor: '',
+                haircolor: '',
+                ethnicity: '',
+                county: {
+                    name: '',
+                    code: '',
+                },
+                birthdate: '',
+                height: 0,
+                weight: 0,
+                year: {
+                    start: 0,
+                    end: 0,
+                },
+            },
         },
         videos: [
             {
@@ -70,6 +171,18 @@ class Star extends Component {
         })
     }
 
+    handleStar_updateInfo(label, value) {
+        Axios.get(`${config.api}/changestarinfo.php?starID=${this.state.star.id}&label=${label}&value=${value}`).then(({ data }) => {
+            if (data.success) {
+                this.setState((prevState) => {
+                    let star = prevState.star
+                    star.info[label] = value
+
+                    return { star }
+                })
+            }
+        })
+    }
     render() {
         return (
             <div className='star-page col-7'>
