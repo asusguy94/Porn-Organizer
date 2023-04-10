92a60dac4adcd8386b167b4c166e2c0c43ebdc59
asusGuy94 <asusguy94.1@gmail.com>
2023-04-10 10:31:14 +0200

    Fixed low MUI development performance

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@
 module.exports = {
   reactStrictMode: false,
   output: 'standalone',
-  eslint: { ignoreDuringBuilds: true }
+  eslint: { ignoreDuringBuilds: true },
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}'
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}'
+    }
+  }
 }
