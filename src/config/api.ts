86bbed8639eec5946655c0fdf6ccd32d0f89412a
asusGuy94 <asusguy94.1@gmail.com>
2024-03-03 11:43:02 +0100

    Changed return type from Promise<Response> to Promise<T>

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,9 +1,28 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 import serverConfig from './server'
 
-export default function createApi(suffix: string) {
-  const baseURL = serverConfig.api + suffix
+type Options = {
+  serverKey: keyof typeof serverConfig
+}
 
-  return { api: axios.create({ baseURL }), baseURL }
+async function getResponse<T>(promise: Promise<AxiosResponse<T>>) {
+  return promise.then(res => res.data)
+}
+
+export default function createApi(suffix: string, options?: Partial<Options>) {
+  const baseURL = serverConfig[options?.serverKey || 'legacyApi'] + suffix
+
+  const api = axios.create({ baseURL })
+
+  return {
+    api: {
+      get: <T>(...args: Parameters<(typeof api)['get']>) => getResponse<T>(api.get(...args)),
+      post: <T>(...args: Parameters<(typeof api)['post']>) => getResponse<T>(api.post(...args)),
+      put: <T>(...args: Parameters<(typeof api)['put']>) => getResponse<T>(api.put(...args)),
+      delete: <T>(...args: Parameters<(typeof api)['delete']>) => getResponse<T>(api.delete(...args))
+    },
+    legacyApi: api,
+    baseURL
+  }
 }
