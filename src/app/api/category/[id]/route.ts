15848f15d95fdfa7048b8149c3b616a4df981978
asusGuy94 <asusguy94.1@gmail.com>
2024-03-03 12:46:31 +0100

    Migrated parseInt to zodValidation

--- src/app/api/category/[id]/route.ts
+++ src/app/api/category/[id]/route.ts
@@ -3,11 +3,18 @@ import { db } from '@utils/server/prisma'
 import validate, { z } from '@utils/server/validation'
 
 export async function PUT(req: Request, { params }: Params<'id'>) {
-  const { name } = validate(z.object({ name: z.string().min(3) }), await req.json())
+  const { id } = validate(z.object({ id: z.number() }), params)
+
+  const { name } = validate(
+    z.object({
+      name: z.string().min(3)
+    }),
+    await req.json()
+  )
 
   return Response.json(
     await db.category.update({
-      where: { id: parseInt(params.id) },
+      where: { id },
       data: { name }
     })
   )
